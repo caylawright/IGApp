9e3541952d4b2a8ec572120377db08a6ce7df0f1
caylawright <cwrigh27@bulldogs.aamu.edu>
2020-10-12 22:28:38 -0500

    ig

---
+++ app/src/main/java/com/example/instagram/ComposeFragment.java
@@ -0,0 +1,36 @@
+package com.example.instagram;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.fragment.app.Fragment;
+
+public class ComposeFragment {
+}
+
+
+@override
+public View onCreateView(LayoutINflater inflater, ViewGroup container, Bundle savedInstanceState){
+    //Inflate the layout for this fragment
+    return inflater.inflate(R.layout.fragment_compose, container, false);
+}
+
+
+public class FooFragment extends bottomNavagation {
+    // The onCreateView method is called when Fragment should create its View object hierarchy,
+    // either dynamically or via XML layout inflation.
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
+        // Defines the xml file for the fragment
+        return inflater.inflate(R.layout.fragment_foo, parent, false);
+    }
+
+    // This event is triggered soon after onCreateView().
+    // Any view setup should occur here.  E.g., view lookups and attaching view listeners.
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        // Setup any handles to view objects here
+        // EditText etFoo = (EditText) view.findViewById(R.id.etFoo);
+    }
+}
